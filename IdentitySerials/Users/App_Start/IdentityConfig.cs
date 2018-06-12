40f3a7f218cd50849ba064c0dbf7ed162ab26ca2
czhensheng <358430429@qq.com>
2018-06-12 19:51:48 +0800

    Users By Identty

---
+++ IdentitySerials/Users/App_Start/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Users.Infrastructure;
+
+//[assembly: Microsoft.Owin.OwinStartup(typeof(Users.App_Start.IdentityConfig))]
+namespace Users.App_Start
+{
+
+    public class IdentityConfig
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<AppIdentityDbContext>(AppIdentityDbContext.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new Microsoft.Owin.PathString("/Account/Login")
+            });
+        }
+    }
+}
---
+++ IdentitySerials/Users/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using Users.Models;
+
+namespace Users.Infrastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDb")
+        {
+
+        }
+
+
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    public class IdentityDbInit
+         : System.Data.Entity.DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSetup(AppIdentityDbContext context)
+        {
+            // initial configuration will go here
+            // 初始化配置将放在这儿
+        }
+    }
+}
